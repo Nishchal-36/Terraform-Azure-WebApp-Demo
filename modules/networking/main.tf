14fa048ee7c7db63ec88f0d2b3a5bd1b787c8e79
NISHCHAL HAWK <84372899+Nishchal-36@users.noreply.github.com>
2025-07-22 01:12:26 +0530

    Module 2: Networking

---
+++ modules/networking/main.tf
@@ -0,0 +1,62 @@
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    Environment = var.environment
+    Component   = "networking"
+  }
+}
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "main" {
+  name                = "app-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Environment = var.environment
+    Component   = "networking"
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
